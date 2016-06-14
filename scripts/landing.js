568c7a0c4dd23e0bd92e2db5d86cea49cafdc443
hcbviolet <hcbviolet@gmail.com>
2016-06-14 14:16:13 -0700

    Began changes

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,7 @@
- var animatePoints = function() {
-
-     var points = document.getElementsByClassName('point');
-     
+//animation isn't working at scroll height!  
+ var pointsArray = document.getElementsByClassName('point');
+ 
+ var animatePoints = function(points) {
      var revealPoint = function() {
          for(var i = 0; i < points.length; i++){
              points[i].style.opacity = 1;
@@ -10,5 +10,16 @@
              points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
         }
      };
-     revealPoint(points);
+     revealPoint(); 
  };
+
+ window.onload = function() {
+     var sellingPoints = document.getElementsByClassName('selling-points')[0];
+     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+     window.addEventListener("scroll", function(event) {
+         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);   
+         }
+     });
+ }
