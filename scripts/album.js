fc13dee8ed4788f3b72c2136f26316bd00acdec8
hcbviolet <hcbviolet@gmail.com>
2016-06-29 11:56:22 -0700

    Add mp3s and buzz library

--- scripts/album.js
+++ scripts/album.js
@@ -21,6 +21,8 @@ var setCurrentAlbum = function(album) {
 var currentlyPlayingSongNumber = null;
 var currentSongFromAlbum = null;
 var currentAlbum = null;
+var currentSoundFile = null;
+var currentVolume = 80;
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
 
@@ -41,7 +43,7 @@ var nextSong = function() {
     }
     
     setSong(currentIndex + 1);
-    
+    currentSoundFile.play();    
     updatePlayerBarSong();
     
     var lastSongNumber = getLastSongNumber(currentIndex);
@@ -64,7 +66,7 @@ var previousSong = function() {
     }
     
     setSong(currentIndex + 1);
-
+    currentSoundFile.play();
     updatePlayerBarSong();
     
     var lastSongNumber = getLastSongNumber(currentIndex);
@@ -95,19 +97,27 @@ var createSongRow = function(songNumber, songName, songLength) {
     
         var clickHandler = function() {
         var songNumber = parseInt($(this).attr('data-song-number'));
+            
         if (currentlyPlayingSongNumber !== null) {
           var currentlyPlayingElement = getSongNumberCell(currentlyPlayingSongNumber);	currentlyPlayingElement.html(currentlyPlayingSongNumber);
         }
         if (currentlyPlayingSongNumber !== songNumber) {
             $(this).html(pauseButtonTemplate);
             setSong(songNumber);
+            currentSoundFile.play();
             updatePlayerBarSong();
         } 
         else if (currentlyPlayingSongNumber === songNumber) {
-            $(this).html(playButtonTemplate);
-            $('.main-controls .play-pause').html(playerBarPlayButton);
-            currentlyPlayingSongNumber = null;
-            currentSongFromAlbum = null;
+            if (currentSoundFile.isPaused()) {
+                $(this).html(pauseButtonTemplate);
+                $('.main-controls .play-pause').html(playerBarPauseButton);
+                currentSoundFile.play();
+            }
+            else if (!currentSoundFile.isPaused()){
+                $(this).html(playButtonTemplate);
+                $('.main-controls .play-pause').html(playerBarPlayButton);
+                currentSoundFile.pause();
+            };
         } 
     };
 
@@ -147,10 +157,23 @@ var updatePlayerBarSong = function() {
 };
 
 var setSong = function(songNumber) {
+    if (currentSoundFile) {
+        currentSoundFile.stop();
+    }
     currentlyPlayingSongNumber = parseInt(songNumber);
     currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+    currentSoundFile = new buzz.sound(currentSongFromAlbum.audioUrl, {
+        formats: [ 'mp3' ],
+        preload: true
+     });
+    setVolume(currentVolume);
 };
 
+var setVolume = function(volume) {
+    if (currentSoundFile) {
+        currentSoundFile.setVolume(volume);
+    }
+};
 var getSongNumberCell = function(number) {
     return $('.song-item-number[data-song-number="' + number + '"]');
 };
